18dbad3ab97c5b73cda615c4cd289b2f9f0fad6f
TeunMayer <2204468mayer@zuyd.nl>
2023-03-31 10:12:17 +0200

    Add project files.

---
+++ FeedBUFapplicatie/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace FeedBUFapplicatie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_login = new System.Windows.Forms.Button();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.button_exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtusername
+            // 
+            this.txtusername.BackColor = System.Drawing.Color.Cyan;
+            this.txtusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtusername.Location = new System.Drawing.Point(87, 123);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(260, 38);
+            this.txtusername.TabIndex = 0;
+            this.txtusername.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.BackColor = System.Drawing.Color.Cyan;
+            this.txtpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpassword.Location = new System.Drawing.Point(87, 246);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(260, 38);
+            this.txtpassword.TabIndex = 1;
+            this.txtpassword.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.TextBox2_MaskInputRejected);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(81, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // button_login
+            // 
+            this.button_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_login.Location = new System.Drawing.Point(87, 357);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(260, 60);
+            this.button_login.TabIndex = 4;
+            this.button_login.Text = "Log in";
+            this.button_login.UseVisualStyleBackColor = true;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // button_clear
+            // 
+            this.button_clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_clear.Location = new System.Drawing.Point(87, 291);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(260, 60);
+            this.button_clear.TabIndex = 5;
+            this.button_clear.Text = "Clear";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // button_exit
+            // 
+            this.button_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_exit.Location = new System.Drawing.Point(87, 423);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(260, 60);
+            this.button_exit.TabIndex = 6;
+            this.button_exit.Text = "Exit";
+            this.button_exit.UseVisualStyleBackColor = true;
+            this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(203)))), ((int)(((byte)(249)))), ((int)(((byte)(254)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(456, 540);
+            this.Controls.Add(this.button_exit);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.button_login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.MaskedTextBox txtpassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.Button button_exit;
+    }
+}
+
---
+++ FeedBUFapplicatie/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FeedBUFapplicatie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            if (txtusername.Text == "student" && txtpassword.Text == "student")
+            {
+                new Form2().Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("The username or password is incorrect, try again");
+                txtusername.Clear();
+                txtpassword.Clear();
+                txtusername.Focus();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            txtusername.Clear();
+            txtpassword.Clear();
+            txtusername.Focus();
+        }
+
+        private void button_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
